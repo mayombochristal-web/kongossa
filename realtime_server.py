a9dd114b5f56b59acb6eb15beac9705d2ed86cfb
mayombochristal-web <mayombochristal@gmail.com>
2026-02-23 14:31:09 +0100

    Create realtime_server.py

---
+++ realtime_server.py
@@ -0,0 +1,21 @@
+import asyncio
+import websockets
+
+clients = set()
+
+async def handler(ws):
+    clients.add(ws)
+    try:
+        async for message in ws:
+            for c in clients:
+                if c != ws:
+                    await c.send(message)
+    finally:
+        clients.remove(ws)
+
+async def main():
+    async with websockets.serve(handler, "localhost", 8765):
+        print("KONGOSSA Realtime Server ON")
+        await asyncio.Future()
+
+asyncio.run(main())
